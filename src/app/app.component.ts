7c629357a99ac8dd9c74fccfdc313af823067bc5
Ayoub CHAABAT <chaabat.02@gmail.com>
2025-02-07 10:12:48 +0100

    Login && Register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'temp-project';
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly USER_KEY = 'user';
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem(this.USER_KEY);
+  }
+
+  login(user: any): void {
+    localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.USER_KEY);
+  }
+
+  getCurrentUser(): any {
+    const user = localStorage.getItem(this.USER_KEY);
+    return user ? JSON.parse(user) : null;
+  }
+}
